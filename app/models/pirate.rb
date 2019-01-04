b59c3001bf4f9c89922d8dbf7e788129993824d7
endotnick <endotnick@gmail.com>
2019-01-04 13:54:34 -0800

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+  attr_accessor :name, :height, :weight, :ships
+
+  @@pirates = []
+  def initialize(attributes)
+    attributes.each do |k, v|
+      send("#{k}=", v)
+    end
+    self.class.all << self
+  end
+
+  def self.all
+    @@pirates
+  end
 end
